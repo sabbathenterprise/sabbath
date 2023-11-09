00cd1ab8045fe683c44e433193939e4d59fbe51f
admin <admin@Senas-MacBook-Pro.local>
2023-11-09 10:40:45 +0300

    final commit

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ const projectContent = {
       desc: " luxurious coastal estate that exudes elegance and offers stunning ocean views, perfect for refined living.",
     },
     {
-      featuredImg: "/projects/Project3.jpg",
+      featuredImg: "/projects/Project3.jpeg",
       title: "Zenith Sofa Collection",
       desc: "A sofa that represents the pinnacle of comfort, offering the highest quality in modern living room furniture.",
     },
